4bfae8ec442fdd3f18b8d19c34a2814bbfb2a469
Teekey <hegrecomm@gmail.com>
2024-02-03 21:54:02 +0900

    feat: Implement getglobal when global is function

--- Compiler/Machine.cpp
+++ Compiler/Machine.cpp
@@ -1,5 +1,6 @@
 #include "Machine.h"
 #include "Instruction.h"
+#include "Kind.h"
 #include <any>
 
 using std::any;
@@ -13,6 +14,8 @@ struct StackFrame {
 
 static vector<StackFrame> callStack;
 
+static auto pushOperand(any) -> void;
+
 auto execute(tuple<vector<Code>, map<string, size_t>> objectCode) -> void {
   callStack.emplace_back();
   auto codeList = get<0>(objectCode);
@@ -20,10 +23,26 @@ auto execute(tuple<vector<Code>, map<string, size_t>> objectCode) -> void {
   while (true) {
     auto code = codeList[callStack.back().instructionPointer];
     switch (code.instruction) {
-    case Instruction::Exit:
+    case Instruction::Call: {
+      break;
+    }
+    case Instruction::GetGlobal: {
+      auto name = toString(code.operand);
+      if (functionTable.count(name))
+        pushOperand(functionTable[name]);
+      else
+        pushOperand(nullptr);
+      break;
+    }
+    case Instruction::Exit: {
       callStack.pop_back();
       return;
     }
+    }
     callStack.back().instructionPointer++;
   }
+}
+
+static auto pushOperand(any operand) -> void {
+  callStack.back().operandStack.push_back(operand);
 }
